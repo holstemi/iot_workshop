902bfedd6caea27869d20b11171613de27fd4149
izvest <eolsti@gmail.com>
2019-01-23 19:16:42 +0200

    bugfix

--- fake_sensor/index.js
+++ fake_sensor/index.js
@@ -3,10 +3,15 @@ const axios = require('axios');
 const round = num => Math.round(num * 100) / 100;
 
 axios.post('http://localhost:3001/api/newreading', {
-  name: 'YEET',
+  name: 'Timo Teekkari',
   temperature: round(17 + (5 * Math.random())),
   pressure: round(8500 + (2500 * Math.random())),
   humidity: round(14 + (76 * Math.random()))
 })
   .then(() => console.log('success'))
-  .catch(err => console.log('Error:', err.response.data));
+  .catch(err => {
+    if (!err.response) {
+      return console.log('Cannot connect to server')
+    }
+    console.log('Error:', err.response.data)
+  });
